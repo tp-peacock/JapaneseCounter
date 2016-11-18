3e25d9986d524ef66d726ad1fd39323e3670aae8
Thomas Peacock <newlocaluser@imac11.complex.ucl.ac.uk>
2016-11-18 14:52:07 +0000

    handles 1-100 if number does not contain zeroes

--- JapaneseCounter.py
+++ JapaneseCounter.py
@@ -10,15 +10,26 @@ def readnumber():
 
 def numsplit():
 	number = readnumber()
-	if len(number) == 1:
-		print onetoten(number)
-	if len(number) == 2:
-		print type(number[0])
-		print onetoten(number[0]),"-",onetoten(number[1])
+	jnumber = ""
+	#print len(number)
+	while len(number) > 0:			
+		if len(number) == 1:
+			jnumber += singledigits(number)
+			number = number[1:]
+			return jnumber
+		if len(number) == 2:
+			jnumber += tens(number[0])
+			number = number[1:]
+		if len(number) == 3:
+		 	jnumber += hundreds(number[0])
+		 	number = number[1:]
 	
-def onetoten(n):
-	str(n)
-	numbers = { "1": "ichi",
+	#	else:
+	#		return "Too large a number to process!"
+	
+def singledigits(n):
+	numbers = { "0":"",
+				"1": "ichi",
 	 			"2": "ni",
 	 			"3": "san",
 	 			"4": "yon", 
@@ -26,8 +37,43 @@ def onetoten(n):
 	 			"6": "roku", 
 	 			"7": "nana", 
 	 			"8": "hachi", 
-	 			"9": "kyuu", 
-	 			"10": "juu"}
-	return numbers[n]
+	 			"9": "kyuu"}
+	return(" "+numbers[n])
 
-numsplit()
+def tens(n):	
+	numbers = { "0":"",
+				"1": "juu",
+	 			"2": "ni",
+	 			"3": "san",
+	 			"4": "yon", 
+	 			"5": "go", 
+	 			"6": "roku", 
+	 			"7": "nana", 
+	 			"8": "hachi", 
+	 			"9": "kyuu"}
+	if n == "1": return(" "+numbers[n])
+	else: return(" "+numbers[n]+"-"+numbers["1"])
+	
+def hundreds(n):
+	numbers = { "0":"",
+				"1": "yaku",
+	 			"2": "nih",
+	 			"3": "sanh",
+	 			"4": "yonh", 
+	 			"5": "goh", 
+	 			"6": "ropp", 
+	 			"7": "nanah", 
+	 			"8": "happ", 
+	 			"9": "kyuuh"}
+	if n == "1": return("h"+numbers[n])
+	else: return(numbers[n]+numbers["1"])
+
+def thousands(n):
+	return 0
+
+print(numsplit())
+
+
+
+
+
